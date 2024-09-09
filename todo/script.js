a2c50950e9d40e973b7ace6d85f9cc394d6a7b3a
Yosi <Yosi@YosiWebiks.com>
2024-09-09 17:59:51 +0300

    modal works, no edit

--- todo/script.js
+++ todo/script.js
@@ -10,6 +10,10 @@
 const inp = document.querySelector('.controls input')
 const btn = document.querySelector('.controls button')
 const list = document.querySelector('.list')
+const editInp = document.querySelector('.edit-inp')
+const modal = document.querySelector('.modal-wraper')
+const closeModal = document.querySelector('.close-modal')
+const saveEditBtn = document.querySelector('.save-modal')
 
 //
 const saveToLocalStorage = title => {
@@ -62,7 +66,21 @@ const createToDoSectionFromEvent = () => {
 const createToDoSectionFromMemory = (td) => createToDoSection(td.title, td.checked)
 
 const addParentHover = e => e.target.parentElement.parentElement.classList.add("hovered")
+const addParentHoverEdt = e => e.target.parentElement.parentElement.classList.add("edt-hovered")
 const rmParentHover = e => e.target.parentElement.parentElement.classList.remove("hovered")
+const rmParentHoverEdt = e => e.target.parentElement.parentElement.classList.remove("edt-hovered")
+
+// open modal
+const openEditModal = e => {
+    modal.classList.add('flex-center')
+    modal.classList.remove('hidden')
+}
+
+// open modal
+const closeEditModal = e => {
+    modal.classList.remove('flex-center')
+    modal.classList.add('hidden')
+}
 
 // create section func
 const createToDoSection = (title, checked = false) => {
@@ -81,6 +99,7 @@ const createToDoSection = (title, checked = false) => {
     checked && td.classList.add('completed')
     td.textContent = title
     const actnWrap = document.createElement('div')
+    actnWrap.classList.add('flex-center')
     const del = document.createElement('p')
     del.classList.add('del')
     del.classList.add('flex-center')
@@ -89,18 +108,27 @@ const createToDoSection = (title, checked = false) => {
     del.addEventListener('click', delToDo)
     del.addEventListener('mouseenter', addParentHover)
     del.addEventListener('mouseleave', rmParentHover)
-
+    const edit = document.createElement('p')
+    edit.textContent = '✏️'
+    edit.classList.add('edt')
+    edit.classList.add('flex-center')
+    edit.addEventListener('click', openEditModal)
+    edit.addEventListener('mouseenter', addParentHoverEdt)
+    edit.addEventListener('mouseleave', rmParentHoverEdt)
+    
     // structure
     lbl.appendChild(cb)
     lbl.appendChild(td)
     tdWrap.appendChild(lbl)
     actnWrap.appendChild(del)
+    actnWrap.appendChild(edit)
     tdWrap.appendChild(actnWrap)
     list.appendChild(tdWrap)
 }
 
 //  event handler
 btn.addEventListener('click', createToDoSectionFromEvent)
-
+saveEditBtn.addEventListener('click', e => {closeEditModal()})
+closeModal.addEventListener('click', closeEditModal)
 
 loadFromLocalStorage()
